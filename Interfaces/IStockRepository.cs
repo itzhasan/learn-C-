5ea187c8a3ee7de7530b0ad513f4c33823c79fce
Hasan Satar <78432339+itzhasan@users.noreply.github.com>
2024-08-03 12:53:32 +0300

    implemen

--- Interfaces/IStockRepository.cs
+++ Interfaces/IStockRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using learn.Dtos.Stock;
 using Learn.Models;
 
 namespace learn.Interfaces
@@ -9,6 +10,9 @@ namespace learn.Interfaces
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync();
-        Task<Stock>GetByIdAsync(int id);
+        Task<Stock?>GetByIdAsync(int id);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
     }
 }
